bf8ff1301e8826004b0c6c5895180b9ff5029a48
John Wang <wangzhiqiang02@inspur.com>
2022-01-14 08:52:03 +0800

    vim-run-ut: Remove the ansi-color
    Signed-off-by: John Wang <wangzhiqiang02@inspur.com>

--- tools/vim-run-ut.sh
+++ tools/vim-run-ut.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
 
-function run-ut()
+TEST_ARG=$1
+
+function run-ut-test-only()
 {
 	REPO="$VIM_ROOT"
 	UT_PATH="${_WORKSPACE_}/openbmc-build-scripts"
-	UNIT_TEST_PKG="$(basename $REPO)" WORKSPACE="$(dirname $REPO)" \
+	UNIT_TEST_PKG="$(basename "$REPO")" WORKSPACE="$(dirname "$REPO")" \
 		NO_FORMAT_CODE=1 TEST_ONLY=1  "$UT_PATH/run-unit-test-docker.sh"
 
 }
 
-run-ut
+function run-ut()
+{
+	REPO="$VIM_ROOT"
+	UT_PATH="${_WORKSPACE_}/openbmc-build-scripts"
+	UNIT_TEST_PKG="$(basename "$REPO")" WORKSPACE="$(dirname "$REPO")" \
+		"$UT_PATH/run-unit-test-docker.sh"
+
+}
+
+
+function main()
+{
+	if [ x"$TEST_ARG" = x"test-only" ];then
+		run-ut-test-only
+	else
+		run-ut
+	fi
+}
+
+# Remove the ansi color https://superuser.com/questions/380772/removing-ansi-color-codes-from-text-stream
+main | sed 's/\x1b\[[0-9;]*[mGKF]//g' 
