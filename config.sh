6fef179ea42e3a68bc731b6e1c75ab28461cda99
John Wang <wangzq.jn@gmail.com>
2022-01-22 18:15:19 +0800

    Move the path env to config.sh
    Signed-off-by: John Wang <wangzq.jn@gmail.com>

---
+++ config.sh
@@ -0,0 +1,11 @@
+
+# 将个人 ~/.local/bin 目录加入 PATH
+if [ -d "$HOME/.local/bin" ]; then
+	export PATH="$HOME/.local/bin:$PATH"
+fi
+
+if [ -f "$HOME/.cargo/env" ]; then
+	. "${HOME}/.cargo/env"
+fi
+
+
--- init.sh
+++ init.sh
@@ -5,15 +5,6 @@ else
 	return
 fi
 
-# 将个人 ~/.local/bin 目录加入 PATH
-if [ -d "$HOME/.local/bin" ]; then
-	export PATH="$HOME/.local/bin:$PATH"
-fi
-
-if [ -f "$HOME/.cargo/env" ]; then
-	. "${HOME}/.cargo/env"
-fi
-
 if [ -f "$HOME/.local/etc/config/bootstrap-gen/config.sh" ]; then
 	. "${HOME}/.local/etc/config/bootstrap-gen/config.sh"
 fi
