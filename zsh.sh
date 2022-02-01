a74403788a9d56c62854ae380aabab5a92f6b5e2
John Wang <wangzq.jn@gmail.com>
2022-02-01 23:47:55 +0800

    zsh: Use antigen as the plugin manager tool
    Signed-off-by: John Wang <wangzq.jn@gmail.com>

--- zsh.sh
+++ zsh.sh
@@ -1,8 +1,21 @@
-export ZSH=$HOME/.oh-my-zsh
-ZSH_THEME="robbyrussell"
 
-# for vi-mode
-KEYTIMEOUT=500
+SHELL_FOLDER=$(dirname "$0")
 
-plugins=(vscode z fzf jsontools vi-mode)
-source $ZSH/oh-my-zsh.sh
+source $SHELL_FOLDER/tools/antigen.zsh
+
+antigen use oh-my-zsh
+
+antigen bundle command-not-found
+antigen use oh-my-zsh
+antigen theme robbyrussell
+
+antigen bundle skywind3000/z.lua
+antigen bundle zsh-users/zsh-completions
+antigen bundle zsh-users/zsh-autosuggestions
+ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+ZSH_AUTOSUGGEST_USE_ASYNC=1
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle ael-code/zsh-colored-man-pages
+
+antigen apply
+
