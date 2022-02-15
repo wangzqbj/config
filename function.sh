a18206219e8c4e98110c895c089d2887fddaf9b2
John Wang <wangzq.jn@gmail.com>
2022-02-15 14:50:37 +0800

    copy-bmc-image: Add a paramer for machine
    Signed-off-by: John Wang <wangzq.jn@gmail.com>

--- function.sh
+++ function.sh
@@ -5,17 +5,19 @@ fi
 
 function copy-bmc-image()
 {
-	dest=$1
-	FP5280G2_BMC_IMAGE_PATH="${_WORKSPACE_}/openbmc/build/fp5280g2/tmp/deploy/images/fp5280g2/image-bmc"
-	if cp "${FP5280G2_BMC_IMAGE_PATH}" "${dest}"; then
-		echo "copy ${FP5280G2_BMC_IMAGE_PATH} to ${dest} successfully"
+	local machine
+	local dest
+	machine=$1
+	dest=$2
+
+	IMAGE_PATH="${_WORKSPACE_}/openbmc/build/$machine/tmp/deploy/images/$machine/image-bmc"
+	if cp "${IMAGE_PATH}" "${dest}"; then
+		echo "copy ${IMAGE_PATH} to ${dest} successfully"
 	else
-		echo -e "\033[31mcopy ${FP5280G2_BMC_IMAGE_PATH} to ${dest} failed\033[0m"
+		echo -e "\033[31mcopy ${IMAGE_PATH} to ${dest} failed\033[0m"
 		exit 1
 	fi
 }
-
-alias run-qemu-new-image="copy-bmc-image ./ && obmc-qemu ./image-bmc"
 
 function weather()
 {
