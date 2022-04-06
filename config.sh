92177bb27c151efdfd9175200d49518062486204
John Wang <wangzq.jn@gmail.com>
2022-04-06 19:11:59 +0800

    config.sh: format
    Signed-off-by: John Wang <wangzq.jn@gmail.com>

--- config.sh
+++ config.sh
@@ -1,4 +1,3 @@
-
 export EDITOR=vim
 
 # 将个人 ~/.local/bin 目录加入 PATH
@@ -6,8 +5,11 @@ if [ -d "$HOME/.local/bin" ]; then
 	export PATH="$HOME/.local/bin:$PATH"
 fi
 
-if [ -f "$HOME/.cargo/env" ]; then
-	. "${HOME}/.cargo/env"
+if [ -d "$HOME/.local/go" ]; then
+	export GOPATH="$HOME/.local/go"
+	if [ -d "$HOME/.local/go/bin" ]; then
+		export PATH="$HOME/.local/go/bin:$PATH"
+	fi
 fi
 
 export UBUNTU_MIRROR=http://mirrors.aliyun.com/ubuntu/
@@ -16,7 +18,6 @@ export PATH="$PATH:$HOME/.vim/plugged/vim-superman/bin"
 
 export FZF_DEFAULT_COMMAND='rg --files --hidden --follow'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-
 
 if [ $TERM = "xterm-kitty" ]; then
 	alias ls='ls --color=tty --hyperlink=auto'
